c04e89d4c08d42253ef2f99be78382d60f675364
mmanteli <amanda.myntti@gmail.com>
2025-03-03 13:42:19 +0200

    First test running

---
+++ run_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -A project_462000615
+#SBATCH -p dev-g
+#SBATCH --ntasks-per-node=1
+##SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=20G
+#SBATCH -t 00:30:00
+#SBATCH -N 1
+#SBATCH -J test_sentence_transformers
+#SBATCH -o logs/%x.out
+
+module purge
+module use /appl/local/csc/modulefiles/
+module load pytorch/2.4
+export PYTHONPATH=/scratch/project_462000615/mynttiam/embedding-extraction/pythonuserbase/lib/python3.10/site-packages:$PYTHONPATH
+
+export TRANSFORMERS_CACHE=/scratch/project_462000615/hf_cache
+export HF_HOME=/scratch/project_462000615/hf_cache
+export HF_DATASETS_CACHE=/scratch/project_462000615/hf_cache
+srun python test_sentence_transformers.py
