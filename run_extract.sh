a826a24dc9d0382320ab4a62edd76fd52a5cdc1e
mmanteli <amanda.myntti@gmail.com>
2025-04-10 10:35:48 +0300

    Parametrisation for register datasets

--- run_extract.sh
+++ run_extract.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 #SBATCH -A project_462000883
-#SBATCH -p dev-g
+#SBATCH -p small-g
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20G
-#SBATCH -t 00:15:00
+#SBATCH -t 02:30:00
 #SBATCH -N 1
-#SBATCH -J test_extract
+#SBATCH -J extract
 #SBATCH -o logs/%x-%j.out
 
+register=$1
 module purge
 module use /appl/local/csc/modulefiles/
 module load pytorch/2.4
@@ -18,5 +19,5 @@ export PYTHONPATH=/scratch/project_462000883/amanda/embedding-extraction/pythonu
 
 export HF_HOME=/scratch/project_462000883/hf_cache
 echo Starting at $(date +%H:%M.%S)
-srun python extract.py --model="e5" --batchsize=64 --split_by="sentences" < ../data/tests/NA_1000.jsonl
+srun python extract.py --model="e5" --split_by="sentences" --save="/scratch/project_462000883/amanda/embedded-data/e5/${register}_test.pkl" < /scratch/project_462000353/HPLT-REGISTERS/samples-150B-by-register-xlmrl/original_corrected/eng_Latn_${register}.jsonl
 echo Ending at $(date +%H:%M.%S)
