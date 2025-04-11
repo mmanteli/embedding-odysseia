7deab2be04e674dff661f94229275c02d9326158
mmanteli <amanda.myntti@gmail.com>
2025-04-11 15:39:29 +0300

    parametrisation and corrections

--- indexing/build_index.py
+++ indexing/build_index.py
@@ -1,6 +1,6 @@
 import pickle
 from sqlitedict import SqliteDict
-
+import sys
 
 def yield_from_pickle(f_name):
     with open(f_name,"rb") as f:
@@ -11,13 +11,15 @@ def yield_from_pickle(f_name):
             except EOFError:
                 break
 
-db = SqliteDict("SP.sqlite")
-input_file = "/scratch/project_462000883/amanda/embedded-data/e5/SP_test.pkl"
+register = sys.argv[1]
+assert register in ["dtp", "ne", "OP", "SP"]
+db = SqliteDict(f"{register}.sqlite")
+input_file = f"/scratch/project_462000883/amanda/embedded-data/e5/{register}_test.pkl"
 
 for i, beet in enumerate(yield_from_pickle(input_file)):
     for b in beet:
         db[b["id"]+"-"+str(b["offset"])] = b
-    if i%10000==0:
+    if i%10000==0:   # not optimized, but results in at least okay performance
         db.commit()
 db.commit()
 
--- indexing/read_index.py
+++ indexing/read_index.py
@@ -1,6 +1,6 @@
 from sqlitedict import SqliteDict
 import sys
-db = SqliteDict("/scratch/project_462000883/amanda/embedding-extraction/SP.sqlite")
+db = SqliteDict("/scratch/project_462000883/amanda/embedding-extraction/dtp.sqlite")
 
 
 def find_full_text(db, index):
@@ -9,12 +9,11 @@ def find_full_text(db, index):
     register=None
     while True:
         try:
-            (current_ind)
             next_id = index+"-"+str(current_ind)
             d = db[next_id]
             t_new = d["text"]
             t += " "+t_new
-            current_ind += len(t_new)
+            current_ind += len(t_new)   # next index is num_characters away
             if register is None:
                 register = d["register"]
         except KeyError as e:
@@ -26,17 +25,24 @@ def find_full_text(db, index):
 #print(len(db))
 
 
-id_to_find = "dc50a7f155d07395def419c55effc5ef"
+id_to_find = "5c808fedd393e4f586bb67bb3955c024"
 print(find_full_text(db, id_to_find))
 
+"""DTP
+"5be7eabe95a0ee2f7653dd0f5f27e724"   #500k does not exist anymore
+"5c808fedd393e4f586bb67bb3955c024"  # 200k exists
+"bae0444728f600bd2c6e1241acad36e6"  # 100k exists
+"4d34b0b66901fb4d302beeb689fef359"  # 50k
 
 """
+
+"""SP
 acd08c41898719c33162fab7fe926a6b   # 1M   # does not exist anymore
 85c2c7ff5d9e891edd6490f4f27e76b5   # 500k  # does not exist anymore
 d9418f1011d54947a51d94c281c1e384   # 250k  # does not exist any 
 fa79034b1d6ae0d9508a4fd3f2c49628  # 100k # does not exist anymore
-6b82c8e3f434a8466cfa6ae9d7941ec1  # 90k
-dc50a7f155d07395def419c55effc5ef # 80k
+6b82c8e3f434a8466cfa6ae9d7941ec1  # 90k # does not exist anymore
+dc50a7f155d07395def419c55effc5ef # 80k # does not exist anymore
 f57f3676ccfd80b7ed54685b9094ca0c  # 79k
 9c681a976e051cc9d2cd6c9cd9aa7432  # 78k
 b77c0616e37dd77b79be26c1c15897b2  # 75k
