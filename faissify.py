de7d2b066d54d06ad09f698e1da90fc3f70a4edd
mmanteli <amanda.myntti@gmail.com>
2025-04-16 11:11:06 +0300

    Semi-working implementation, not fully tested

--- faissify.py
+++ faissify.py
@@ -24,12 +24,15 @@ def make_training_sample(options, fraction = 0.1):
     Save the data to options.training_data, return True on success
     """
     if options.debug: print(f'Making a training set with fraction {fraction}', flush=True)
-    training_embeddings = []
+    training_embeddings = None
     try:
         for beet in yield_from_pickle(options.data):
             for b in beet:
                 if np.random.random() < fraction:
-                    training_embeddings.append(b["embeddings"])
+                    if isinstance(training_embeddings, np.ndarray):
+                        training_embeddings = np.vstack((training_embeddings, b["embeddings"]))
+                    else:
+                        training_embeddings = b["embeddings"]
         torch.save(training_embeddings, options.training_data)
         return True
     except:
@@ -47,39 +50,50 @@ def train_faiss(options):
         index = faiss.IndexIVFPQ(quantizer, options.embedding_dim, options.num_cells, options.num_quantizers, options.quantizer_bits)
     else:
         # using only FlatL2
-        if debug: print("Using FlatL2 as the full indexer.", flush=True)
+        if options.debug: print("Using FlatL2 as the full indexer.", flush=True)
         index = quantizer
     if not index.is_trained:
         training_vectors = torch.load(options.training_data)
         if options.debug: print(f'Training {options.base_indexer} on {len(training_vectors)} vectors.', flush=True)
-        index.train(training_vectors.numpy())
+        index.train(training_vectors)
         if options.debug: print(f'Training done. Saving to {options.trained_indexer}.', flush=True)
         index_trained = index
         faiss.write_index(index_trained, options.trained_indexer)
         return index
-    return index
+    else:
+        if options.debug: print("This index does not require training.", flush=True)
+        return index
         
 
-def extract_embedding_and_id(d):
-    return d["embeddings"], d["id"]
+def extract_embedding_and_id(data):
+    ids = []
+    embs = []
+    for d in data:
+        ids.append(int(d["id"],16))
+        embs.append(d["embeddings"])
+    #return embs, ids  #TODO
+    return embs, ids
 
 def index_w_fais(options, index=None):
     if index is None:
-        if debug: print(f"Reading {options.trained_indexer}...", flush=True)
+        if options.debug: print(f"Reading {options.trained_indexer}...", flush=True)
         index = faiss.read_index(options.trained_indexer)
     # collect batches
     emb_to_index = []
     id_to_index = []
-    for i, beet in enumerate(yield_from_pickle(options.data)):
-        emb_, id_ = extract_embedding_and_id(d)
-        emb_to_index.append(emb_)
-        id_to_index.append(id_)
+    i = 0
+    for beet in yield_from_pickle(options.data):
+        emb_, id_ = extract_embedding_and_id(beet)
+        emb_to_index.extend(emb_)
+        id_to_index.extend([i])
         if i%1000 == 0:  # for every batch
-            index.add_with_ids(emb_to_index.numpy(), id_to_index.numpy())
+            E, I = np.vstack([np.array(e) for e in emb_to_index]), np.array(id_to_index)
+            index.add_with_ids(E, I)
+            # reinit
             emb_to_index = []
             id_to_index = []
     # tail values
-    index.add_with_ids(emb_to_index.numpy(), id_to_index.numpy())
+    index.add_with_ids(emb_to_index, id_to_index)
     index_filled = index
     faiss.write_index(index_filled, options.filled_indexer)
     if options.debug: print(f'Filling done, {index_filled.ntotal} vectors in index.', flush=True)
@@ -89,6 +103,7 @@ def index_w_fais(options, index=None):
 def run(options):
     # check if the training data is already done
     if not pathlib.Path(options.training_data).is_file():
+        if options.debug: print("Did not find training data, making it from options.data.")
         data_exists = make_training_sample(options)
         if not data_exists:
             print("Could not construct training data, see error above.", flush=True)
@@ -118,8 +133,10 @@ if __name__ == "__main__":
     parser.add_argument('--quantizer_bits', type=int, help="How many bits used to save quantizer values", default = 8)
     parser.add_argument('--debug', type=bool, default=True, help="Verbosity etc.")
 
+    options = parser.parse_args()
+    print(options, flush=True)
     # option checks
-    assert ".pkl" in options.data or ".pickle" in options.data, "Give training data in pickled json format."
+    assert ".pkl" in options.data or ".pickle" in options.data, "Give data in pickled json format."
     if options.training_data:
         assert ".pt" in options.training_data, "Training data does not have .pt extension."
     run(options)
---
+++ run_faissify.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -A project_462000883
+#SBATCH -p debug
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=20G
+#SBATCH -t 00:30:00
+#SBATCH -N 1
+#SBATCH -J faissify
+#SBATCH -o logs/%x-%j.out
+
+module purge
+module use /appl/local/csc/modulefiles/
+module load pytorch/2.4
+export PYTHONPATH=/scratch/project_462000883/amanda/embedding-extraction/pythonuserbase/lib/python3.10/site-packages:$PYTHONPATH
+export HF_HOME=/scratch/project_462000883/hf_cache
+
+srun python faissify.py --base_indexer="IVFPQ" \
+                        --trained_indexer="/scratch/project_462000883/amanda/embedding-extraction/trained-indexers/first_test.index" \
+                        --filled_indexer="/scratch/project_462000883/amanda/embedding-extraction/filled-indexers/first_test.index" \
+                        --data="/scratch/project_462000883/amanda/embedded-data/e5/dtp_test.pkl" \
+                        --training_data="/scratch/project_462000883/amanda/embedding-extraction/training-data/first_test.pt" \
+                        
