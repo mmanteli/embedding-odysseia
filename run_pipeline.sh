5214c9fcca4588dda57c6322764533aee411618f
mmanteli <amanda.myntti@gmail.com>
2025-05-02 10:24:48 +0300

    Commit before major revision

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=20G
 #SBATCH -t 00:30:00
 #SBATCH -N 1
-#SBATCH -J pipeline-test-sanity-duplicates
+#SBATCH -J pipeline-index-full-docs-sanity
 #SBATCH -o logs/%x-%j.out
 
 
@@ -16,8 +16,6 @@ module use /appl/local/csc/modulefiles/
 module load pytorch/2.4
 export PYTHONPATH=/scratch/project_462000883/amanda/embedding-extraction/pythonuserbase/lib/python3.10/site-packages:$PYTHONPATH
 
-# this will be piped in to extract.py
-data_to_embed="/scratch/project_462000883/amanda/combined-testset/duplicates.jsonl"
 
 # what action to take
 action=$1
@@ -30,22 +28,28 @@ case $action in
         exit 1
 esac
 
+REGISTER=$2
+
+jobname="full_docs"
+# this will be piped in to extract.py
+#data_to_embed="/scratch/project_462000883/amanda/register-data/${REGISTER}.jsonl"
+data_to_embed="/scratch/project_462000353/HPLT-REGISTERS/samples-150B-by-register-xlmrl/original_corrected/eng_Latn_${REGISTER}.jsonl"
 # Embedding extraction related options
 model="e5"
 task="STS"
-data="/scratch/project_462000883/amanda/embedded-data/e5/pipeline/duplicates.pkl"  # location to save the embeddings
+data="/scratch/project_462000883/amanda/embedding-extraction/embedded-data/e5/full_docs/" #"/scratch/project_462000883/amanda/embedding-extraction/embedded-data/e5/${jobname}/${REGISTER}.pkl"  # location to save the embeddings
 shard=0   #we use the first index in data (can be commaseparated list)
-temporary_training_set="/scratch/project_462000883/amanda/embedding-extraction/training-data/duplicates.pkl"
-threshold=0.2
-split_by="sentences"
+temporary_training_set="/scratch/project_462000883/amanda/embedding-extraction/training-data/full_docs/" #"/scratch/project_462000883/amanda/embedding-extraction/training-data/${jobname}/${REGISTER}.pkl"
+threshold=0.1
+split_by="truncate"
 chunk_size=2500
 
 # indexing with faiss related options
-base_indexer="IVFPQ"
-training_data="/scratch/project_462000883/amanda/embedding-extraction/training-data/duplicates.pt"
-trained_indexer="/scratch/project_462000883/amanda/embedding-extraction/trained-indexers/duplicates.index"
-filled_indexer="/scratch/project_462000883/amanda/embedding-extraction/filled-indexers/duplicates.index"
-database="/scratch/project_462000883/amanda/embedding-extraction/indexed-data/duplicates.sqlite"
+base_indexer="HNSW"
+training_data="/scratch/project_462000883/amanda/embedding-extraction/training-data/${jobname}-${base_indexer}.pt"
+trained_indexer="/scratch/project_462000883/amanda/embedding-extraction/trained-indexers/${jobname}-${base_indexer}.index"
+filled_indexer="/scratch/project_462000883/amanda/embedding-extraction/filled-indexers/${jobname}-${base_indexer}.index"
+database="/scratch/project_462000883/amanda/embedding-extraction/indexed-data/${jobname}-${base_indexer}.sqlite"
 # Verbosity
 debug="True"
 
@@ -60,6 +64,7 @@ case $action in
                     --model=$model \
                     --data=$data \
                     --temp=$temporary_training_set \
+                    --split_by=$split_by \
                     --threshold=$threshold \
                     --debug=$debug < $data_to_embed
     ;;
