c055fc7de2cb46a494b5ae26795c0fd17770e777
mmanteli <amanda.myntti@gmail.com>
2025-05-13 13:56:37 +0300

    Dummy examples working

--- hidden-states/extract.py
+++ hidden-states/extract.py
@@ -1,9 +1,13 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
+import json
+import pandas as pd
 from jsonargparse import ArgumentParser
+import datasets
+from torch.utils.data import DataLoader
 
 ap = ArgumentParser(prog="test.py")
-ap.add_argument('--model', default="/scratch/project_462000883/amanda/register-models/dtp/iter_0050000")
+ap.add_argument('--model', default="/scratch/project_462000883/amanda/register-models/OP/iter_0050000")
 ap.add_argument('--tokenizer', default="gpt2")
 ap.add_argument('--layers', default=-1)
 ap.add_argument('--data')
@@ -14,41 +18,88 @@ ap.add_argument('--save')
 
 options = ap.parse_args()
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-text ="Hello, I am a freelance"
+print(f'Using {device}')
+# load model and tokenizer
 model = AutoModel.from_pretrained(options.model)
 tokenizer = AutoTokenizer.from_pretrained(options.tokenizer)
 if type(options.layers) is int:
     options.layers = [options.layers]
+print("Model and tokenizer loaded.")
+# load data
+#ds = datasets.load_dataset('json', data_files=options.data)
+#print(ds)
+#ds = ds["train"].select(range(10))
+#print("dataset loaded")
+#print(ds)
 
+# load data
+data = []
+with open(options.data) as f:
+    for line in f:
+        data.append(json.loads(line))
 
+data = data[:10]
+# functions for embedding :)
 def tokenize(t):
     """Tokenize a piece of text."""
     if t is None:
         return tokenizer("", return_tensors='pt', truncation=True)
     return tokenizer(t, return_tensors='pt', truncation=True)
 
+
+# Does not work because padding token does not exist!
+def tokenize_fn(t):
+    """Tokenize a dict with field 'text'."""
+    if t is None:
+        return tokenizer("", return_tensors='pt', truncation=True, padding=True)
+    return tokenizer(t["text"], return_tensors='pt', truncation=True, padding=True)
+
 def flatten_by_tokens(state):
     """Flatten a model layer output wrt. tokens. Tokens are in dimension -2."""
-    return torch.mean(state, axis=-2)
+    return torch.mean(state, axis=-2).numpy().tolist()
 
-def extract(model, text, layers=None):
+def extract(model, tokenized, layers):
     """Get hidden states of model for given layers."""
-    if layers is None:
-        layers=[-1]
+    tokenized.to(model.device)
     with torch.no_grad():
-        output = model(**tokenize(text), return_dict_in_generate=True, output_hidden_states=True)
+        output = model(**tokenized, return_dict_in_generate=True, output_hidden_states=True)
     hidden_states = output["hidden_states"]
+    del output
+    return_value = [flatten_by_tokens(hidden_states[i]) for i in layers]
+    del hidden_states
     torch.cuda.empty_cache()
-    return [flatten_by_tokens(hidden_states[i]) for i in layers]
+    return return_value
 
 
-tokenized_input = tokenize(text).input_ids
-print(f'lenght of tokenized input prompt = {len(tokenized_input[0])}, {tokenized_input}')
 
-hidden_states_flattened = extract(model, text, layers=[-1])
-# print(len(hidden_states_flattened), hidden_states_flattened[0].shape) # this is correct now
+#ds = ds.map(tokenize_fn, batched=True)
+#print("tokenized")
+#ds.set_format(type="torch", columns=["input_ids", "attention_mask"])
+#loader = DataLoader(ds, batch_size=4)
+#for batch in loader:
+#    print("new_batch")
+#    batch = {k: v.to(model.device) for k, v in batch.items()}
+#    output = extract(model, batch, layers=options.layers)
 
 
+results = []
+for t in data:
+    print("new iteration")
+    tokenized = tokenize(t["text"])
+    print("\tNow embedding")
+    output = extract(model, tokenized, options.layers)
+    print("\tEmbedded, saving...")
+    assert len(options.layers) == len(output)
+    embed_results = {}
+    for o,l in zip(output, options.layers):
+        embed_results[f"layer_{l}"] = o
+    results.append({**t, **embed_results})
+
+print("embedding done")
+df = pd.DataFrame.from_dict(results)
+print(df)
+df.to_csv(options.save)
+#ds.save_to_disk(options.save)
 
 
 
--- hidden-states/kernel_divergence.py
+++ hidden-states/kernel_divergence.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 # test cases:
 # normalize (0.57, 0.483, 0.53) = (0.62225851, 0.52728221, 0.57859124)
 # normalize (1,1,0) = (0.70710678, 0.70710678, 0.0)
@@ -28,18 +30,53 @@ def l2_squared(vector):
 def radial_basis(z, gamma, normalize=True):
     """Calculate the similarity matrix between the same set of vectors."""
     if normalize:
-        normalize_vectors(z)
+        z = normalize_vectors(z)
+        assert l2_squared(z[0]) -1.0 < 1e-8, f'norm is {l2_squared(z[0])}'
     result = np.zeros((z.shape[0], z.shape[0]))
     for i,v1 in enumerate(z):
         for j,v2 in enumerate(z):
+            print(v1-v2)
+            print(l2_squared(v1-v2))
             result[i,j] = np.exp(-gamma*l2_squared(v1-v2))
+    return result
 
 
 def kernel_divergence(phi, psi):
     """Evaluate changes between phi (baseline) and psi (target) radial basis matrises."""
     normalizer = np.sqrt(phi.sum())
-    distance_matrix = np.log(phi/psi)
+    distance_matrix = phi - psi #np.log(phi/psi)   # TODO this only works with gamma=1
     combined = phi * distance_matrix
     score = combined.sum()/normalizer
     return distance_matrix, combined, score
 
+df_OP = pd.read_csv("/scratch/project_462000883/amanda/embedding-odysseia/hidden-states/testi_OP.csv")
+df_LY = pd.read_csv("/scratch/project_462000883/amanda/embedding-odysseia/hidden-states/testi_LY_with_OP.csv")
+df = pd.concat([df_OP, df_LY])
+
+#vecs = np.array(df["layer_-1"].tolist())
+vecs_OP = np.array([eval(i) for i in df_OP["layer_-1"]])
+vecs_LY = np.array([eval(i) for i in df_LY["layer_-1"]])
+
+vecs_OP = vecs_OP.reshape((10,-1))
+vecs_LY = vecs_LY.reshape((10,-1))
+
+phi = radial_basis(vecs_OP, 1)
+psi = radial_basis(vecs_LY, 1)
+print(phi)
+
+distance, gated, value = kernel_divergence(phi, psi)
+print(value)
+
+hm0 = sns.heatmap(phi)
+figure = hm0.get_figure()
+figure.savefig('base.png', dpi=400)
+plt.clf()
+
+hm1 = sns.heatmap(distance)
+figure = hm1.get_figure()
+figure.savefig('distance.png', dpi=400)
+plt.clf()
+
+hm2 = sns.heatmap(gated)
+figure = hm2.get_figure()
+figure.savefig('gated.png', dpi=400)
