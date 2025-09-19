d39e2add6e0e51213770c1c8eb5cce400f1afc7c
mmanteli <amanda.myntti@gmail.com>
2025-09-19 14:43:49 +0300

    Expanding and tensor dimensions work; indices not quite right

--- bubbles/expand.py
+++ bubbles/expand.py
@@ -12,23 +12,21 @@ parser = ArgumentParser()
 parser.add_argument("--sent1", default="Jack Morris is a PhD student at Cornell Tech in New York City")
 parser.add_argument("--sent2", default="It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity")
 parser.add_argument("--search_iter", default=30, type=int)
+parser.add_argument("--batch_size", default=8, type=int)
 options = parser.parse_args()
 
 def get_gtr_embeddings(text_list,
                        encoder: PreTrainedModel,
                        tokenizer: PreTrainedTokenizer) -> torch.Tensor:
-
     inputs = tokenizer(text_list,
                        return_tensors="pt",
                        max_length=128,
                        truncation=True,
                        padding="max_length",).to("cuda")
-
     with torch.no_grad():
         model_output = encoder(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'])
         hidden_state = model_output.last_hidden_state
         embeddings = vec2text.models.model_utils.mean_pool(hidden_state, inputs['attention_mask'])
-
     return embeddings
 
 
@@ -38,7 +36,7 @@ corrector = vec2text.load_pretrained_corrector("gtr-base")
 
 
 embedding = get_gtr_embeddings([
-       "Jack Morris is a PhD student at Cornell Tech in New York City",
+       ["Jack Morris is a PhD student at Cornell Tech in New York City", "It was the best of times, it was the worst of times, it was the age of wisdom,"]
 ], encoder, tokenizer)
 
 
@@ -62,6 +60,18 @@ def inversion_function(vector_to_be_inverted):
     #print(type(vector_to_be_inverted))
     #print(vector_to_be_inverted.dtype)
     #return ["placeholder"]
+    print(f'Shape of inverted {vector_to_be_inverted.shape}')
+    if vector_to_be_inverted.shape[0]>8:
+        print("\tdoing batch")
+        results = []
+        for batch in vector_to_be_inverted.split(options.batch_size): 
+            r = vec2text.invert_embeddings(
+                                        embeddings=batch.cuda(),
+                                        num_steps=options.search_iter,
+                                        corrector=corrector
+                                    )
+            results.append(r)
+        return results
     return vec2text.invert_embeddings(
                                         embeddings=vector_to_be_inverted.cuda(),
                                         num_steps=options.search_iter,
@@ -103,11 +113,11 @@ class Point:
         return inversion_function(self.loc)
 
     def distance(self, reference_point):
-        return eucdistance.euclidean(self.loc, reference_point)
+        return eucdistance.euclidean(self.loc.squeeze().cpu(), reference_point.squeeze().cpu())
 
 
 class Bubble:
-    def __init__(self, origin, n_dim, delta):
+    def __init__(self, origin, n_dim, n_lines, delta):
         if isinstance(origin, Point):
             self.origin = origin
         else:
@@ -116,57 +126,63 @@ class Bubble:
             except:
                 raise AttributeError("Give point loc as torch.Tensor")
         self.n_dim = n_dim
-        self.n_lines = n_dim
+        self.n_lines = n_lines
         self.axes = self.create_axes()
         assert 0<delta<1, "Give delta-value between (0,1)."
         self.delta = delta
         self.points = self.create_initial_points()
-        self.initial_inversion = self.origin.invert()
-        print(self.initial_inversion)
+        self.initial_inversion = self.origin.invert()[0]
+        print(f'Initial inversion: {self.initial_inversion}')
 
     def create_axes(self):
+        if self.n_lines < self.n_dim:
+            if self.n_lines == 1:
+                indices = np.array([0])
+            else:
+                indices = np.random.choice(self.n_dim, size=self.n_lines, replace=False)
+            print(f'Selected axes: {indices}')
+            return torch.concatenate([torch.eye(self.n_dim, dtype=torch.float32)[indices], -1.*torch.eye(self.n_dim,dtype=torch.float32)[indices]]).cuda()
         return torch.concatenate([torch.eye(self.n_dim, dtype=torch.float32), -1.*torch.eye(self.n_dim,dtype=torch.float32)]).cuda() # both directions
+
     def create_initial_points(self):
-        return [Point(torch.tensor(self.delta*p + self.origin.loc, dtype=torch.float32), i) for i,p in enumerate(self.axes)]
+        return [Point(self.delta*p + self.origin.loc, i%self.n_lines) for i,p in enumerate(self.axes)]
 
     def expand(self):
         for i, point in enumerate(self.points):
             point.move(self.delta*self.axes[i])
 
     def get_points_as_matrix(self):
-        return torch.stack([p.loc for p in self.points])
+        return torch.cat([p.loc for p in self.points if not p.frozen], dim=0)
 
     def invert_points(self):
-        inversions=[]
-        for p in self.points:
-            inversions.append(p.invert())
-        return inversions
-        
-        #return inversion_function(self.get_points_as_matrix())
+        return inversion_function(self.get_points_as_matrix())
     
     def evaluate_points(self):
         inversions = self.invert_points()
         print("Inversions:")
-        print(inversions)
-        for i,p in self.points:
-            if inversions[i] != self.initial_inversion:
-                p.freeze()
+        print(len(inversions), inversions)
+        for i,p in enumerate(self.points):
+            if not p.frozen:
+                print(f'In point {p.idx}')
+                print(f'Index = {i}')
+                if inversions[i] != self.initial_inversion:
+                    print(f'Freezing, because {inversions[i]} != {self.initial_inversion}')
+                    p.freeze()
     
     def loop(self):
         while not all([p.is_frozen for p in self.points]):
             self.evaluate_points()
             self.expand()
         print("All frozen")
-        self.print_points()
+        self.print_distance()
 
-    def print_points(self):
+    def print_distance(self):
         for p in self.points:
-            p.print()
+            print(p.idx, ": ", p.distance(self.origin.loc))
 
+embedding = get_gtr_embeddings([
+       "Jack Morris is a PhD student at Cornell Tech in New York City"],
+        encoder, tokenizer)
 
-print(embedding)
-print(embedding.shape)
-print(type(embedding))
-
-bubble = Bubble(embedding, embedding.shape[1], 0.001)
+bubble = Bubble(embedding, embedding.shape[1], 4, 0.001)
 bubble.loop()
