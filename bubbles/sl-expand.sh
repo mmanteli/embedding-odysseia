978c1861f800d381d3d8f840969594cc07a8d87e
mmanteli <amanda.myntti@gmail.com>
2025-10-22 10:39:30 +0300

    statistics implemented

--- bubbles/sl-expand.sh
+++ bubbles/sl-expand.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH -A project_462000883
-#SBATCH -p dev-g
+#SBATCH -p small-g
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20G
-#SBATCH -t 00:30:00
+#SBATCH -t 12:29:00
 #SBATCH -N 1
-#SBATCH -J expand
+#SBATCH -J expand_synthetic2
 #SBATCH -o logs/%x-%j.out
 
 index=$1
@@ -20,5 +20,5 @@ export PYTHONPATH=/scratch/project_462000883/amanda/pythonuserbase/lib/python3.1
 export HF_HOME=/scratch/project_462000883/hf_cache
 
 echo $(date +"%T")
-python expand.py --embedding_file="seed_embeddings.jsonl" --file_index=$index --savepath="tests/" --delta="0.05" --batch_size=32
+python expand.py --embedding_file="seed_embeddings_synthetic.jsonl" --file_index=$index --savepath="bubbledata/synthetic2/" --delta="0.0005" --batch_size=64
 echo $(date +"%T")
